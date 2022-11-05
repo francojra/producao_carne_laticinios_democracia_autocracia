974d0408ce396101f627083a05c89edd1cdfb13a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-04 23:15:09 -0300

    Add files via upload

---
+++ producao_carne_laticinios_democracia_autocracia.R
@@ -0,0 +1,21 @@
+
+# Produção de carne e laticínios em países democratas e autocratas -------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 04/11/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/meat-production -----------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Alimentar o mundo de uma forma sustentável é um dos nossos desafios mais prementes 
+### nas próximas décadas. A carne desempenha um papel fulcral neste contexto.
+
+### Carne é uma importante fonte de nutrição para muitas pessoas em todo mundo. A demanda
+### global de carne está crescendo: passado 50 anos, a produção de carne tem mais que
+### triplicado. O mundo agora produz mais que 340 milhões de toneladas por ano.
+
+### Mas a produção de carne tem grandes impactos ambientais - aumentando gases de efeito
+### estufa, terras de agricultura e uso de água. Um dos maiores desafios mundiais é
+### produzir e consumir carne, laticínios e outros produtos protéicos de uma forma que
+### reduza os impactos ambientais.
+
+
